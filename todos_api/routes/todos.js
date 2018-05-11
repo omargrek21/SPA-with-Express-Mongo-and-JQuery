5836879bff5ed37893dc1768681b4140cfe1966c
Omar Perez <omarmk6@gmail.com>
2018-05-11 01:59:22 +0000

    Route refactor

--- todos_api/routes/todos.js
+++ todos_api/routes/todos.js
@@ -1,56 +1,15 @@
 var express = require('express'),
     router = express.Router(),
-    db = require('../models');
+    helpers = require('../helpers/todos');
+   
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo);
     
-router.get('/', function(req, res){
-    db.Todo.find()
-    .then(function(todos){
-        res.json(todos);
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
-    
-router.post('/', function(req, res){
-    db.Todo.create(req.body)
-    .then(function(newTodo){
-        res.status(201).json(newTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
-
-router.get('/:todoId', function(req,res){
-   db.Todo.findById(req.params.todoId)
-   .then(function(foundTodo){
-       res.json(foundTodo);
-   })
-   .catch(function(err){
-       res.send(err);
-   });
-});
-
-router.put('/:todoId', function(req,res){
-   db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new:true})
-   .then(function(updatedTodo){
-       res.json(updatedTodo);
-   })
-   .catch(function(err){
-       res.send(err);
-   })
-});
-
-router.delete('/:todoId', function(req,res){
-    db.Todo.remove({_id: req.params.todoId})
-    .then(function(){
-        res.json({message: 'deleted successfully'});
-    })
-    .catch(function(err){
-        res.send(err);
-    });
-});
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo);
     
 module.exports = router;
     
